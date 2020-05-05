df632ab203a0185486fa4fc5713f74b4f2ef8513
valer <valer@LAPTOP-SEKIN>
2020-05-05 14:06:13 +0200

    gh

--- swt-example/swt-example-master/src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/JourneyController.java
+++ swt-example/swt-example-master/src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/JourneyController.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 
 @Controller
 public class JourneyController {
-		//test valerij
+		//test valerij test
 	
     @Autowired
     private JourneyRepository journeyRepository;
